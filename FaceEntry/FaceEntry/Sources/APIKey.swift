a11ff0c33ba4b88925322d34fcd599a6ae303d76
Keith Moon <keef@keefmoon.com>
2017-04-02 01:07:58 +0100

    API Key file

---
+++ FaceEntry/FaceEntry/Sources/APIKey.swift
@@ -0,0 +1,9 @@
+//
+//  APIKey.swift
+//  FaceEntry
+//
+//  Created by Keith Moon on 30/03/2017.
+//  Copyright © 2017 Keith Moon. All rights reserved.
+//
+
+let subscriptionKey = "Enter Microsoft Cognative Services Face API Subscription Key"
